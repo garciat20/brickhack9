6f5c2ce6fbae0535c826df7ec9057a91a5c55b5a
Thomas Garcia <thomasakulla@gmail.com>
2023-02-25 15:31:08 -0500

    skeleton

--- src/db/rest_commands.py
+++ src/db/rest_commands.py
@@ -1,8 +1,22 @@
-import brickhack_db_utils
+from brickhack_db_utils import *
+from getData import *
+
+def setUp():
+    build_tables()
+    insert_data()
+
+def build_tables():
+    exec_sql_file("schema.sql")
+    return
+
+def insert_data():
+    # insert from getData
+    return
+
 
 def query(brands, types):
     sql = """
     SELECT * FROM """
-    items = exec_get_all()
+    items = exec_get_all(sql)
 
 
--- src/server.py
+++ src/server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import db.rest_commands
 
 app = Flask(__name__)
 api = Api(app)
@@ -7,3 +8,4 @@ api = Api(app)
 
 if __name__ == '__main__':
     app.run(debug=True, port=4999)
+    db.rest_commands.setUp()
