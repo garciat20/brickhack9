a39583439ddb4a02ad90c397e475e014eb9f50b2
Thomas Garcia <thomasakulla@gmail.com>
2023-02-25 13:49:37 -0500

    directory for db commands, and added text file to install proper packages

---
+++ src/db/brickhack-db-utils.py
@@ -0,0 +1,37 @@
+import psycopg2
+import yaml
+import os
+
+def connect():
+    psycopg2.connect(
+    database="brickhack9",
+    user="brickhack9",
+    password="brickhack9",
+    host="localhost",
+    port="4999"
+    )
+
+def exec_get_one(sql, args={}):
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute(sql, args)
+    one = cur.fetchone()
+    conn.close()
+    return one
+
+def exec_get_all(sql, args={}):
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute(sql, args)
+    # https://www.psycopg.org/docs/cursor.html#cursor.fetchall
+    list_of_tuples = cur.fetchall()
+    conn.close()
+    return list_of_tuples
+
+def exec_commit(sql, args={}):
+    conn = connect()
+    cur = conn.cursor()
+    result = cur.execute(sql, args)
+    conn.commit()
+    conn.close()
+    return result
